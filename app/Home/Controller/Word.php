9c2066032df53c5288eeea9fe59369bdf1ad1bd5
戒尺 <itxiao6@qq.com>
2017-09-28 14:36:24 +0800

    首页链接修改

---
+++ app/Home/Controller/Word.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Home\Controller;
+
+/**
+ * 文章控制器
+ * Class Word
+ * @package App\Home\Controller
+ */
+class Word extends Base
+{
+    public function detail()
+    {
+        $this -> display();
+    }
+}
