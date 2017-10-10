053ad40c68de08caa3d53108826d327d2eee1ce5
戒尺 <itxiao6@qq.com>
2017-10-10 16:05:25 +0800

    server 模块兼容

--- config/abstract.php
+++ config/abstract.php
@@ -5,6 +5,14 @@ return [
 	'resources'=>ROOT_PATH.'vendor'.DIRECTORY_SEPARATOR.'maximebf'.DIRECTORY_SEPARATOR.'debugbar'.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'DebugBar'.DIRECTORY_SEPARATOR.'resources',
 	# 后台资源文件
 	'assets'=>ROOT_PATH.'common'.DIRECTORY_SEPARATOR.'assets',
+	'css'=>ROOT_PATH.'public'.DIRECTORY_SEPARATOR.'css',
+	'font'=>ROOT_PATH.'public'.DIRECTORY_SEPARATOR.'font',
+	'images'=>ROOT_PATH.'public'.DIRECTORY_SEPARATOR.'images',
+	'js'=>ROOT_PATH.'public'.DIRECTORY_SEPARATOR.'js',
+	'layui'=>ROOT_PATH.'public'.DIRECTORY_SEPARATOR.'layui',
+	'mods'=>ROOT_PATH.'public'.DIRECTORY_SEPARATOR.'mods',
+	'upload'=>ROOT_PATH.'public'.DIRECTORY_SEPARATOR.'upload',
+	''=>ROOT_PATH.'public'.DIRECTORY_SEPARATOR.'',
     # 百度富文本编辑器资源路由
 	'umeditor'=>ROOT_PATH.'common'.DIRECTORY_SEPARATOR.'umeditor',
 ];
