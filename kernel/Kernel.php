900577ff05b6354e878fb5343c8a9e4eafc3aec6
戒尺 <itxiao6@qq.com>
2017-10-18 11:37:06 +0800

    HTTP 类影射

--- kernel/Kernel.php
+++ kernel/Kernel.php
@@ -272,6 +272,7 @@ class Kernel
             $view_path[] = ROOT_PATH.'common/';
             # 设置公用模板
             Config::set('sys',$view_path,'view_path');
+            dd(Config::get('sys','view_path'));
             # 默认的404 页面
             $controller = new Controller;
             # 输出默认的404 页面
