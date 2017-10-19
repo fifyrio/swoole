c41491bbd2d1b2d02bb37b69f2f03f66ccb92049
戒尺 <itxiao6@qq.com>
2017-10-19 17:19:21 +0800

    123123

--- common/assets/js/tab.js
+++ common/assets/js/tab.js
@@ -101,7 +101,7 @@ layui.define(['element', 'common'], function (exports) {
     /**
      * 获取页面内容(并渲染)
      * */
-     $.get(data.href,{globalTabIdIndex:globalTabIdIndex},function(content) use(this){
+     $.get(data.href,{globalTabIdIndex:globalTabIdIndex},function(content) use(that,this){
         var _config = that.config;
         var tabIndex = that.exists(data.title);
         var waitLoadIndex;
