baabaad241cb0de9fbb2e766185a785ce6c250b9
戒尺 <itxiao6@qq.com>
2017-10-19 16:45:31 +0800

    js 渲染导航修改

--- common/assets/js/index.js
+++ common/assets/js/index.js
@@ -8,47 +8,46 @@ layui.config({
 }).use(['element', 'layer', 'navbar', 'tab'], function () {
     var element = layui.element(),
         $ = layui.jquery,
-        layer = layui.layer,
-        navbar = layui.navbar();
-    tab = layui.tab({
-        elem: '.admin-nav-card' //设置选项卡容器
-        ,
-        //maxSetting: {
-        //	max: 5,
-        //	tipMsg: '只能开5个哇，不能再开了。真的。'
-        //},
-        contextMenu: true,
-        onSwitch: function (data) {
-            console.log(data.id); //当前Tab的Id
-            console.log(data.index); //得到当前Tab的所在下标
-            console.log(data.elem); //得到当前的Tab大容器
+        layer = layui.layer;
+        tab = layui.tab({
+            elem: '.admin-nav-card' //设置选项卡容器
+            ,
+            //maxSetting: {
+            //  max: 5,
+            //  tipMsg: '只能开5个哇，不能再开了。真的。'
+            //},
+            contextMenu: true,
+            onSwitch: function (data) {
+                // console.log(data.id); //当前Tab的Id
+                // console.log(data.index); //得到当前Tab的所在下标
+                // console.log(data.elem); //得到当前的Tab大容器
 
-            console.log(tab.getCurrentTabId())
-        },
-        closeBefore: function (obj) { //tab 关闭之前触发的事件
-            console.log(obj);
-            //obj.title  -- 标题
-            //obj.url    -- 链接地址
-            //obj.id     -- id
-            //obj.tabId  -- lay-id
-            if (obj.title === 'BTable') {
-                layer.confirm('确定要关闭' + obj.title + '吗?', { icon: 3, title: '系统提示' }, function (index) {
-                    //因为confirm是非阻塞的，所以这里关闭当前tab需要调用一下deleteTab方法
-                    tab.deleteTab(obj.tabId);
-                    layer.close(index);
-                });
-                //返回true会直接关闭当前tab
-                return false;
-            }else if(obj.title==='表单'){
-                layer.confirm('未保存的数据可能会丢失哦，确定要关闭吗?', { icon: 3, title: '系统提示' }, function (index) {
-                    tab.deleteTab(obj.tabId);
-                    layer.close(index);
-                });
-                return false;
+                // console.log(tab.getCurrentTabId())
+            },
+            closeBefore: function (obj) { //tab 关闭之前触发的事件
+                // console.log(obj);
+                //obj.title  -- 标题
+                //obj.url    -- 链接地址
+                //obj.id     -- id
+                //obj.tabId  -- lay-id
+                if (obj.title === 'BTable') {
+                    layer.confirm('确定要关闭' + obj.title + '吗?', { icon: 3, title: '系统提示' }, function (index) {
+                        //因为confirm是非阻塞的，所以这里关闭当前tab需要调用一下deleteTab方法
+                        tab.deleteTab(obj.tabId);
+                        layer.close(index);
+                    });
+                    //返回true会直接关闭当前tab
+                    return false;
+                }else if(obj.title==='表单'){
+                    layer.confirm('未保存的数据可能会丢失哦，确定要关闭吗?', { icon: 3, title: '系统提示' }, function (index) {
+                        tab.deleteTab(obj.tabId);
+                        layer.close(index);
+                    });
+                    return false;
+                }
+                return true;
             }
-            return true;
-        }
-    });
+        });
     //iframe自适应
     $(window).on('resize', function () {
         var $content = $('.admin-nav-card .layui-tab-content');
@@ -58,21 +57,28 @@ layui.config({
         });
     }).resize();
 
-    //设置navbar
-    navbar.set({
-        spreadOne: true,
-        elem: '#admin-navbar-side',
-        cached: true,
-        data: navs
-		/*cached:true,
-		url: 'datas/nav.json'*/
+    // 绑定导航按钮点击事件
+    $('.layui-nav-child dd a').on('click',function(){
+        if($(this).attr('data-url') != '' && $(this).attr('data-url') != undefined){
+            tab.tabAdd({title:$(this).attr('title'),href:$(this).attr('data-url')});
+        }
     });
+    
+    // //设置navb
+    // navbar.set({
+    //     spreadOne: true,
+    //     elem: '#admin-navbar-side',
+    //     cached: true,
+    //     data: navs
+        // /*cached:true,
+        // url: 'datas/nav.json'*/
+    // });
     //渲染navbar
-    navbar.render();
+    // navbar.render();
     //监听点击事件
-    navbar.on('click(side)', function (data) {
-        tab.tabAdd(data.field);
-    });
+    // navbar.on('click(side)', function (data) {
+    //     tab.tabAdd(data.field);
+    // });
     //清除缓存
     $('#clearCached').on('click', function () {
         navbar.cleanCached();
@@ -186,7 +192,7 @@ function lock($, layer) {
                         $this.attr('type', 'text').val('输入密码解锁..');
                     }
                 });
-            //在此处可以写一个请求到服务端删除相关身份认证，因为考虑到如果浏览器被强制刷新的时候，身份验证还存在的情况			
+            //在此处可以写一个请求到服务端删除相关身份认证，因为考虑到如果浏览器被强制刷新的时候，身份验证还存在的情况          
             //do something...
             //e.g. 
 
@@ -211,11 +217,11 @@ function lock($, layer) {
                 }
                 unlock(userName, pwd);
             });
-			/**
-			 * 解锁操作方法
-			 * @param {String} 用户名
-			 * @param {String} 密码
-			 */
+            /**
+             * 解锁操作方法
+             * @param {String} 用户名
+             * @param {String} 密码
+             */
             var unlock = function (un, pwd) {
                 console.log(un, pwd);
                 //这里可以使用ajax方法解锁
