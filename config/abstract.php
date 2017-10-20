604737299aa143bfdd15c8fb2e3b07ba1cd1bfaf
戒尺 <itxiao6@qq.com>
2017-10-20 13:42:52 +0800

    debugbar 资源路由

--- config/abstract.php
+++ config/abstract.php
@@ -2,7 +2,7 @@
 # 虚拟目录
 return [
 	# debugbar资源文件
-	'Resources'=>ROOT_PATH.'vendor'.DIRECTORY_SEPARATOR.'maximebf'.DIRECTORY_SEPARATOR.'debugbar'.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'DebugBar'.DIRECTORY_SEPARATOR.'Resources',
+	'Resources'=>ROOT_PATH.'vendor'.DIRECTORY_SEPARATOR.'itxiao6'.DIRECTORY_SEPARATOR.'debugbar'.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'DebugBar'.DIRECTORY_SEPARATOR.'Resources',
 	# 后台资源文件
 	'assets'=>ROOT_PATH.'common'.DIRECTORY_SEPARATOR.'assets',
     # 百度富文本编辑器资源路由
