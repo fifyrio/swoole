e7c97ac27dd3bbe1d41d82a74b77da0c3973007c
戒尺 <itxiao6@qq.com>
2017-10-20 11:46:49 +0800

    资源路由大小写 修正

--- config/abstract.php
+++ config/abstract.php
@@ -2,7 +2,7 @@
 # 虚拟目录
 return [
 	# debugbar资源文件
-	'resources'=>ROOT_PATH.'vendor'.DIRECTORY_SEPARATOR.'maximebf'.DIRECTORY_SEPARATOR.'debugbar'.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'DebugBar'.DIRECTORY_SEPARATOR.'resources',
+	'Resources'=>ROOT_PATH.'vendor'.DIRECTORY_SEPARATOR.'maximebf'.DIRECTORY_SEPARATOR.'debugbar'.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'DebugBar'.DIRECTORY_SEPARATOR.'Resources',
 	# 后台资源文件
 	'assets'=>ROOT_PATH.'common'.DIRECTORY_SEPARATOR.'assets',
     # 百度富文本编辑器资源路由
