311c59b4e7b2673036447d29e992af3542e937bb
戒尺 <itxiao6@qq.com>
2017-11-08 14:23:17 +0800

    在首页打印 导航表内的信息

--- app/Home/Controller/Index.php
+++ app/Home/Controller/Index.php
@@ -1,23 +1,20 @@
 <?php
 namespace App\Home\Controller;
 use App\Model\Menu;
-use App\Model\User;
-use Itxiao6\Route\Bridge\Http;
-
 /**
  * 前台首页控制器
  * Class Index
  * @package App\Home\Controller
  */
 class Index extends Base{
-  # 首页操作
-  public function index(){
-      # 打印导航表里的数据
-      $this -> output(function(){
+    # 首页操作
+    public function index(){
+        # 打印导航表里的数据
+        $this -> output(function(){
           var_dump(Menu::get() -> toArray());
-      });
-      return ;
-      #渲染模板
+        });
+        return ;
+        #渲染模板
         $this -> display();
-  }
+    }
 }
