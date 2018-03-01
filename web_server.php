95d69c2228805874abc1ca1e0b77372fad714a04
mi pro <itxiao6@qq.com>
2018-03-01 11:09:01 +0800

    web 服务脚本

--- web_server.php
+++ web_server.php
@@ -1,10 +1,22 @@
 <?php
 # 框架初始化
 require_once('./init.php');
-# 创建一个web服务
-Kernel\Swoole::web_server_start('0.0.0.0',9501)
-    -> set_callback('request',function($request,$response){
-        \App\Http\Kernel::start(\Container\Request::getInterface($request),\Container\Response::getInterface($response));
-    }) -> set_callback('start',function($server){
-        echo "server start，".$server -> host.":".$server -> port."。\n";
-    }) -> start();
+# 选择操作名
+switch ($argv[1]){
+    case 'start':
+        # 创建一个web服务
+        Kernel\Swoole::web_server_start('0.0.0.0',9501)
+            -> set_callback('request',function($request,$response){
+                \App\Http\Kernel::start(\Container\Request::getInterface($request),\Container\Response::getInterface($response));
+            }) -> set_callback('start',function($server){
+                echo "server start，".$server -> host.":".$server -> port."。\n";
+            }) -> start(isset($argv[2])&&$argv[2]=='--d');
+        break;
+    case 'stop':
+        # 创建一个web服务
+        Kernel\Swoole::web_server_start('0.0.0.0',9501,[],false)
+            -> stop((isset($argv[2])&&$argv[2]>0)?$argv[2]:9501);
+        break;
+}
+
+
